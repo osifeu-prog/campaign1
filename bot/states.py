1b5acc067685ce90542ce00a42831a3b248aed3d
Osif ungar <osif.e.u@gmail.com>
2026-01-09 22:35:58 +0200

    Create states.py

---
+++ bot/states.py
@@ -0,0 +1,18 @@
+# ===============================
+# מצבי ConversationHandler
+# ===============================
+
+(
+    CHOOSING_ROLE,
+    SUPPORTER_NAME,
+    SUPPORTER_CITY,
+    SUPPORTER_EMAIL,
+    SUPPORTER_PHONE,
+    SUPPORTER_FEEDBACK,
+    EXPERT_NAME,
+    EXPERT_FIELD,
+    EXPERT_EXPERIENCE,
+    EXPERT_POSITION,
+    EXPERT_LINKS,
+    EXPERT_WHY,
+) = range(12)
