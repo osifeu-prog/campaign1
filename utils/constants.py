87ec3a1967c8f039b9d42d25bd9c920762f631fc
Osif ungar <osif.e.u@gmail.com>
2026-01-10 15:10:25 +0200

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -18,15 +18,24 @@ SUPPORT_GROUP_ID = os.getenv("SUPPORT_GROUP_ID", "")
 ROLE_SUPPORTER = "supporter"
 ROLE_EXPERT = "expert"
 
+# מספר מקומות
+MAX_POSITIONS = int(os.getenv("MAX_POSITIONS", "121"))
+
+# תיקיית תמונות לפתיחת /start
+START_IMAGES_DIR = os.getenv("START_IMAGES_DIR", "media/start_slides")
+
 # callback data קבועים לתפריטים
 CALLBACK_MENU_MAIN = "menu_main"
 CALLBACK_MENU_SUPPORT = "menu_support"
 CALLBACK_MENU_EXPERT = "menu_expert"
 CALLBACK_MENU_ADMIN = "menu_admin"
+
 CALLBACK_APPLY_EXPERT = "apply_expert_again"
 CALLBACK_APPLY_SUPPORTER = "apply_supporter"
+
 CALLBACK_ADMIN_PENDING_EXPERTS = "admin_pending_experts"
 CALLBACK_ADMIN_GROUPS = "admin_groups"
+
 CALLBACK_ADMIN_SHEETS = "admin_sheets"
 CALLBACK_ADMIN_SHEETS_INFO = "admin_sheets_info"
 CALLBACK_ADMIN_SHEETS_FIX = "admin_sheets_fix"
@@ -38,3 +47,9 @@ CALLBACK_ADMIN_EXPORT = "admin_export"
 CALLBACK_ADMIN_QUICK_NAV = "admin_quick_nav"
 
 CALLBACK_MENU_POSITIONS = "menu_positions"
+
+# קרוסלת פתיחה /start
+CALLBACK_START_SLIDE = "start_slide"          # start_slide:<index>
+CALLBACK_START_SOCI = "start_sociocracy"      # מסך הסוציוקרטיה
+CALLBACK_START_SOCI_BACK = "start_soci_back"  # חזרה מהסוציוקרטיה לקרוסלה
+CALLBACK_START_FINISH = "start_finish"        # מעבר למסך "ברוך הבא"
