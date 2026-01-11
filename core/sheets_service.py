e44177be28ab73a5ae8d28f5bf3815ee7d68027b
Osif ungar <osif.e.u@gmail.com>
2026-01-11 20:49:47 +0200

    Update sheets_service.py

--- core/sheets_service.py
+++ core/sheets_service.py
@@ -1,1 +1,21 @@
-core/sheets_service.py
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+
+SCOPE = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
+
+def get_client():
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        "service_account.json", SCOPE
+    )
+    return gspread.authorize(creds)
+
+def get_sheet(tab_name: str):
+    client = get_client()
+    sheet = client.open_by_key(SHEET_ID)
+    return sheet.worksheet(tab_name)
