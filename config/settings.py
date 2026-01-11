af2c143cc6f11eb4d3161c10ddd8c687b53abfb1
Osif ungar <osif.e.u@gmail.com>
2026-01-12 01:14:36 +0200

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -1,17 +1,4 @@
-import os  # ← חובה, כדי להשתמש ב-os.getenv
-
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-
-GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
-GOOGLE_SHEETS_SPREADSHEET_ID = os.getenv("GOOGLE_SHEETS_SPREADSHEET_ID")
-
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-LOG_GROUP_ID = os.getenv("LOG_GROUP_ID")
-
-PORT = int(os.getenv("PORT", 8080))
-
-if not TELEGRAM_BOT_TOKEN:
-    raise RuntimeError("TELEGRAM_BOT_TOKEN is not set")
+import os
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+PORT = int(os.getenv('PORT',8080))
