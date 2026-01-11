d5d2dd3144cdbb05abda87fd01022056c42511d9
Osif ungar <osif.e.u@gmail.com>
2026-01-11 20:54:12 +0200

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,16 +1,21 @@
-import os
 from telegram.ext import Application
-from handlers.start import start_handler
+from handlers.registration import register_handler
+from handlers.admin import approve_handler
+from handlers.experts import expert_handler
+from handlers.images import image_handler, resize_handler
 from handlers.help_all import all_handler
-from handlers.image_resize import image_message_handler
-from handlers.image_permission import permission_request_handler
+import os
 
-def start_app():
-    app = Application.builder().token(os.environ["TELEGRAM_BOT_TOKEN"]).build()
+def start_webhook():
+    app = Application.builder().token(
+        os.getenv("TELEGRAM_BOT_TOKEN")
+    ).build()
 
-    app.add_handler(start_handler)
+    app.add_handler(register_handler)
+    app.add_handler(approve_handler)
+    app.add_handler(expert_handler)
+    app.add_handler(image_handler)
+    app.add_handler(resize_handler)
     app.add_handler(all_handler)
-    app.add_handler(permission_request_handler)
-    app.add_handler(image_message_handler)
 
     app.run_polling()
